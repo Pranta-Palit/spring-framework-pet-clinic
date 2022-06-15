7350a3ac3c70c8d036b77f970139821cc4b965d0
Pranta-Palit <ppalit.cghs@gmail.com>
2022-06-15 19:42:21 +0600

    Initial Project

---
+++ src/test/java/com/bjit/petclinic/SpringFrameworkPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjit.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
